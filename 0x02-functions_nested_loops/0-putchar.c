7d3a16093eebabadb6e3022a1e3e1bf4cd0ad2c5
Chigozie Emeribe <71635581+jonadabbanks@users.noreply.github.com>
2022-04-13 05:21:04 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- * main - main function
+ * main - Entry point
  *
- * Return: End program
+ * Return: nothing, return void
  */
+
 int main(void)
 {
-	char string[8] = "_putchar";
-	int n = 0;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	while (n < 8)
-	{
-		_putchar(string[n]);
-		++n;
-	}
-
-	_putchar(10);
-	return (0);
-
+return (0);
 }
